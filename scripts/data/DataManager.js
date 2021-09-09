dfee5a24e48ef8d09f8e242735259c9391252ecd
Kaitlyn Long <katielong11@gmail.com>
2021-09-09 14:45:42 -0500

    created a DataManager.js file and a function that will fetch journalEntries

---
+++ scripts/data/DataManager.js
@@ -0,0 +1,8 @@
+export const getjournalEntries = () => {
+    return fetch("http://localhost:8088/")
+    .then(response => response.json())
+    .then(parsedResponse => {
+        return parsedResponse;
+    })
+}
+
