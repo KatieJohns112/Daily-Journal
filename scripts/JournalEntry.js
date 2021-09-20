1661890582e8ec3d728aed0bb0a3e7f183425765
Kaitlyn Long <katielong11@gmail.com>
2021-09-20 15:53:57 -0500

    began writing functions for submit entry button and did additional css

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -1,4 +1,9 @@
 export const journalEntryComponent = (entry) => {
-    return `<section id="entry-${entry.id}" class="journalEntry"></section>`
+    return `<section id="entry-${entry.id}" class="journalEntry">
+        date:${entry.date} 
+        concept:${entry.concept} 
+        entry:${entry.entry}
+        mood:${entry.mood}</section>`
+
 }
 
--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -5,6 +5,8 @@ const entryLog = document.querySelector(".entryLog");
 export const entryListComponent = () => {
     const entries= getJournalEntries()
     for (const entry of entries) {
+        console.log(entry)
         entryLog.innerHTML += journalEntryComponent(entry)
     }
 }
+
