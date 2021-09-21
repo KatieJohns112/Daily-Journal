6009b3ecbb02892ebd2d9a23330c79da33d22566
Kaitlyn Long <katielong11@gmail.com>
2021-09-21 16:50:04 -0500

    fixed merging errors in github

--- scripts/JournalEntry.js
+++ scripts/JournalEntry.js
@@ -7,3 +7,11 @@ export const journalEntryComponent = (entry) => {
 
 }
 
+// export const JournalEntryComponent = (entry) => {
+//     return `
+//         <section id="entry--${entry.id}" class="journalEntry">
+//             Display the entry's full text, and the date
+//             it was entered here.
+//         </section>
+//     `
+// }
